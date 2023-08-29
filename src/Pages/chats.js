50fe15d09feb869650134e78f64e56c14ef6a551
Daniela Odhiambo <98682114+DanielaOdhis@users.noreply.github.com>
2023-08-29 20:29:30 +0300

    Add files via upload

--- src/Pages/chats.js
+++ src/Pages/chats.js
@@ -4,6 +4,8 @@ const Chats = () => {
   const [chatData, setChatData] = useState([]);
   const [selectedProfileChats, setSelectedProfileChats] = useState([]);
   const [showProfilePopup, setShowProfilePopup] = useState(false);
+  const [showInputField, setShowInputField] = useState(false);
+  const [inputText, setInputText] = useState("");
 
   useEffect(() => {
     fetch('/chats.json')
@@ -12,13 +14,41 @@ const Chats = () => {
       .catch(error => console.error('Error fetching chat data:', error));
   }, []);
 
+    useEffect(() => {
+    const handleEscapeKeyPress = (e) => {
+      if (e.key === 'Escape') {
+        setShowProfilePopup(false);
+        setSelectedProfileChats([]);
+        setShowInputField(false);
+      }
+    };
+
+    document.addEventListener('keydown', handleEscapeKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleEscapeKeyPress);
+    };
+  }, []);
+
   const handleLastMessageClick = (profileChats) => {
     setSelectedProfileChats(profileChats);
+    setShowInputField(true);
     console.log("Chat Clicked");
   };
 
-  const handleProfileClick = (profile) => {
+  const handleProfileClick = () => {
     setShowProfilePopup(!showProfilePopup);
+  };
+
+   const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSendClick = () => {
+    if (inputText) {
+      console.log("User typed:", inputText);
+      setInputText("");
+    }
   };
 
   return (
@@ -54,25 +84,48 @@ const Chats = () => {
         ))}
       </div>
       <div className={`chat-list-content ${showProfilePopup ? 'blurred' : ''}`}>
-      {showProfilePopup && selectedProfileChats && (
-        <div className="profile-popup-overlay" >
-          <div className="profile-popup">
-            <div className="profile-popup-content">
-              <img
-                src={selectedProfileChats.profile}
-                alt="Profile"
-                className="profile-popup-image"
-              />
+        {showProfilePopup && selectedProfileChats && (
+          <div className="profile-popup-overlay" >
+            <div className="profile-popup">
+              <div className="profile-popup-content">
+                <img
+                  src={selectedProfileChats.profile}
+                  alt="Profile"
+                  className="profile-popup-image"
+                />
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
         <h1>Let's Chat</h1>
         {selectedProfileChats.map((message, index) => (
           <div key={index} className={`chat-message ${message.senderId === 1 ? 'S-sent' : 'R-received'}`}>
             {message.message}
           </div>
         ))}
+        {showInputField && (
+          <div className="input-field">
+            <input type="text" className="input"  value={inputText}  onChange={handleInputChange}/>
+           {inputText && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="send-icon"
+                onClick={handleSendClick}
+              >
+                <line x1="22" y1="2" x2="11" y2="13"></line>
+                <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
+              </svg>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
